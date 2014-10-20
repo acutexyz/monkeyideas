ce31887c8f8b6e90a76559634469138d1a6ac44d
Aidan <aidanxyz@gmail.com>
2014-10-20 17:08:09 +0000

    Add files to repo

---
+++ app/models.py
@@ -0,0 +1,149 @@
+from app import db
+from datetime import datetime
+from passlib.hash import md5_crypt
+from app.utils import enum
+
+class Profession(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    monkeys = db.relationship('Monkey', backref='profession', lazy='dynamic')
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return self.name
+    
+class Monkey(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(64))
+    fullname = db.Column(db.String(100))
+    about = db.Column(db.String(200))
+    profession_id = db.Column(db.Integer, db.ForeignKey('profession.id'))
+    is_public = db.Column(db.Boolean, default=True)
+    birthyear = db.Column(db.Integer) #
+    sex = db.Column(db.Boolean) #
+    facebook = db.Column(db.String(100)) #
+    twitter = db.Column(db.String(100)) #
+    ideas = db.relationship('Idea', backref='author', lazy='dynamic')
+    requests = db.relationship('JoinRequest', backref='monkey', lazy='dynamic')
+    suggestions = db.relationship('Suggestion', backref='monkey', lazy='dynamic')
+        
+    def __init__(self, email, fullname, about, profession_id):
+        self.email = email
+        self.fullname = fullname
+        self.about = about
+        self.profession_id = profession_id
+        
+    def set_password(self, password):
+        self.password = md5_crypt.encrypt(password) # if md5_crypt.verify(raw, hash):
+    
+    def __repr__(self):
+        return self.fullname
+    
+    # Following four methods are for Flask-Login
+    def is_authenticated(self):
+        return True
+    
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+fields = db.Table('fields',
+                  db.Column('idea_id', db.ForeignKey('idea.id')),
+                  db.Column('field_id', db.ForeignKey('field.id')),
+                 )
+
+class Field(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return self.name
+    
+members = db.Table('members', 
+                   db.Column('idea_id', db.ForeignKey('idea.id')),
+                   db.Column('monkey_id', db.ForeignKey('monkey.id')),
+                  )
+
+class IdeaStatus(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True)
+    ideas = db.relationship('Idea', backref='status', lazy='dynamic')
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return self.name
+    
+
+class Idea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('monkey.id'))
+    fields = db.relationship('Field', secondary=fields, backref=db.backref('ideas', lazy='dynamic'))
+    monkeys = db.relationship('Monkey', secondary=members, backref=db.backref('memberin', lazy='dynamic'))
+    is_public = db.Column(db.Boolean, default=True)
+    status_id = db.Column(db.Integer, db.ForeignKey('idea_status.id'))
+    requests = db.relationship('JoinRequest', backref='idea', lazy='dynamic')
+    suggestions = db.relationship('Suggestion', backref='idea', lazy='dynamic')
+    
+    def __init__(self, title, body, author_id, is_public):
+        self.title = title
+        self.body = body
+        self.author_id = author_id
+        self.is_public = is_public
+        
+    def __repr__(self):
+        return self.title
+    
+JoinRequestStatus = enum(
+    SENT=0,
+    ACCEPTED=1,
+    DECLINED=2
+)
+    
+class JoinRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    monkey_id = db.Column(db.Integer, db.ForeignKey('monkey.id'))
+    idea_id = db.Column(db.Integer, db.ForeignKey('idea.id'))
+    status = db.Column(db.Integer, default=JoinRequestStatus.SENT) # default?
+    date_sent = db.Column(db.DateTime)
+    message = db.Column(db.String(200))
+    
+    def __init__(self, monkey_id, idea_id, message=""):
+        self.monkey_id = monkey_id
+        self.idea_id = idea_id
+        self.date_sent = datetime.utcnow()
+        self.message = message
+        self.status = JoinRequestStatus.SENT
+        
+    def __repr__(self):
+        return self.monkey + ' -> "' + self.idea + '"'
+    
+class Suggestion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    monkey_id = db.Column(db.Integer, db.ForeignKey('monkey.id'))
+    idea_id = db.Column(db.Integer, db.ForeignKey('idea.id'))
+    date_sent = db.Column(db.DateTime)
+    
+    def __init__(self, monkey_id, idea_id, message=""):
+        self.monkey_id = monkey_id
+        self.idea_id = idea_id
+        self.date_sent = datetime.utcnow()
+        self.message = message
+        
+    def __repr__(self):   
+        return '"' + self.idea + '" -> ' + self.monkey
+    
---
+++ app/views.py
@@ -0,0 +1,69 @@
+from app import app, models
+from flask.ext.login import LoginManager, login_user, logout_user, login_required
+from flask import request
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(userId):
+    return models.Monkey.query.get(userId)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    pass
+
+@app.route('/register', methods=['GET', 'POST'])
+@login_required
+def register():
+    pass
+
+@app.route('/monkeys', methods=['GET'])
+@login_required
+def list_monkeys():
+    pass
+
+@app.route('/monkeys/<int:id>', methods=['GET'])
+@login_required
+def show_monkey(id):
+    pass
+
+@app.route('/edit_profile', methods=['GET', 'POST'])    # /monkeys/<int:id>/edit
+@login_required
+def edit_profile():
+    pass
+
+@app.route('/ideas', methods=['GET'])
+@login_required
+def list_ideas():
+    pass
+
+@app.route('/ideas/<int:id>', methods=['GET'])
+@login_required
+def show_idea(id):
+    pass
+
+@app.route('/ideas/new', methods=['GET', 'POST'])
+@login_required
+def add_idea():
+    pass
+
+@app.route('/ideas/<int:id>/edit', methods=['GET'])
+@login_required
+def edit_idea(id):
+    pass
+
+@app.route('/monkeys/<int:monkey_id>/suggest', methods=['GET', 'POST'])
+@login_required
+def suggest_to_user(user_id):
+    pass
+
+@app.route('/ideas/<int:idea_id>/join', methods=['GET', 'POST'])
+@login_required
+def request_to_join(idea_id):
+    pass

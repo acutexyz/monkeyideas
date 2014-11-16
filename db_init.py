aaf3c62e75f097c6a6c78b40af8756f08f6b6565
Aidan <aidanxyz@gmail.com>
2014-11-16 19:58:15 +0000

    included IdeaStatuses in db_init script

--- db_init.py
+++ db_init.py
@@ -1,5 +1,5 @@
 from app.factory import create_app
-from app.models import db, Field, Profession
+from app.models import db, Field, Profession, IdeaStatus
 import os
 
 app = create_app(os.getcwd() + '/config.py')
@@ -8,6 +8,7 @@ ctx = app.app_context()
 ctx.push()
 
 db.app = app
+db.drop_all()
 db.create_all()
 
 fields = ['E-commerce/Commerce', 'Education', 'Medicine', 'Social networks', 'Automation Solutions', 'Finance']
@@ -21,6 +22,15 @@ professions = ['Software Engineer', 'Designer', 'Business/Marketing', 'Investor'
 for name in professions:
     profession = Profession(name)
     db.session.add(profession)
+
+idea_statuses = ['Just in mind. No further step is done yet.',
+                'Made a research and proved the idea is good.',
+                'I already implemented it, I need a team.',
+                'Product is ready. So is my team. Need investors.']
+
+for name in idea_statuses:
+    status = IdeaStatus(name)
+    db.session.add(status)
     
 db.session.commit()
 
